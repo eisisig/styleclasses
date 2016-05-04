ecdc4742d97367eeb599b566e013155eb4a1b0e8
Eisi Sig <eisi@meniga.is>
2016-05-04 11:14:15 +0000

    Adding single class return if condition object is provided

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 'use strict'
 
 const get = require('lodash.get')
-const isEmpty = require('lodash.isempty')
+const find = require('lodash.find')
+const keys = require('lodash.keys')
+const every = require('lodash.every')
 const isArray = require('lodash.isarray')
+const isEmpty = require('lodash.isempty')
+const isObject = require('lodash.isobject')
 const kebabCase = require('lodash.kebabcase')
 
-function styleclasses (styles) {
+function styleclasses ( styles ) {
 
 	if ( !styles ) return console.warn('Please provide a styles object from a css file')
 
 	const s = styles
 
-	return (key, params, extra) => {
+	return ( key, params, extra ) => {
 
 		let c = ''
 
@@ -24,6 +28,7 @@ function styleclasses (styles) {
 						const v = params[k]
 						if ( v === true ) {
 							const modKey = `${key}--${kebabCase(k)}`
+
 							c += ` ${get(s, k) || ''}`
 							c += ` ${get(s, modKey) || ''}`
 						}
@@ -31,20 +36,38 @@ function styleclasses (styles) {
 				}
 			}
 		}
-			
+
 		// if key is array
 		else if ( isArray(key) ) {
-			key.forEach((k) => c += ` ${get(s, k) || ''}`)
+			key.forEach(( k ) => {
+
+				if ( typeof k === 'string' ) {
+					c += ` ${get(s, k) || ''}`
+				}
+				// values are arrays or objects
+				else {
+					const match = find(key, ( value ) => every(value))
+
+					if ( match ) {
+						const key = isObject(match) && isArray(match) ? match[0] : keys(match)[0]
+						const style = get(styles, key)
+
+						if ( style ) {
+							c = style
+						}
+					}
+				}
+			})
 		}
 
 		// Extra
 		if ( !isEmpty(extra) && isArray(extra) ) {
-			extra.forEach((k) => c += ` ${get(s, k) || ''}`)
+			extra.forEach(( k ) => c += ` ${get(s, k) || ''}`)
 		} else if ( typeof extra === 'string' ) {
 			c += ` ${extra}`
 		}
 
-		return c
+		return c.trim()
 	}
 }
 
--- styleclasses-test.js
+++ styleclasses-test.js
@@ -1,1 +1,60 @@
 // TODO
+
+const test = require('ava')
+const styleclasses = require('./index')
+
+const styles = {
+	busy: 'Test__busy',
+	disabled: 'Test__disabled',
+	active: 'Test__is-active',
+	normal: 'Test__normal',
+}
+
+const sx = styleclasses(styles)
+
+// Test__disabled
+test('should return a single class', t => {
+	t.is(sx('disabled'), 'Test__disabled')
+})
+
+// Test__normal Test__is-active
+test('should return a single class and extra modifier', t => {
+	t.is(sx('normal', { active: true }), 'Test__normal Test__is-active')
+})
+
+// Test__normal items special-items
+test('should return a single class and 2 extra classes', t => {
+	t.is(sx('normal', null, 'items special-items'), 'Test__normal items special-items')
+})
+
+// Test__normal Test__busy Test__disabled"
+test('should return a multiple class', t => {
+	t.is(sx(['normal', 'busy', 'disabled']), 'Test__normal Test__busy Test__disabled')
+})
+
+test('should return a single class after finding first truthy', t => {
+	const classes = sx([
+		{ active: true },
+		{ busy: true },
+		{ disabled: true },
+	])
+	t.is(classes, 'Test__is-active')
+})
+
+test('should return a single class after finding second truthy', t => {
+	const classes = sx([
+		{ active: false },
+		{ busy: true },
+		{ disabled: true },
+	])
+	t.is(classes, 'Test__busy')
+})
+
+test('should return a single class after finding second truthy using array', t => {
+	const classes = sx([
+		['active', false],
+		['busy', false],
+		['disabled', true],
+	])
+	t.is(classes, 'Test__disabled')
+})
